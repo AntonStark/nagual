9ff7b178eb1751fc1bfd2a7daa1666311a8fb01b
Anton Yakovlev <lantar23@yandex.ru>
2019-04-07 23:53:10 +0300

    remove redundant files

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data'
 
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
-);
+class App extends Component {
+    render() {
+        return (
+            <h1>Hi men</h1>
+        )
+    }
+}
 
-export default App;
+export default withTracker(() => {
+    return {};
+})(App);
